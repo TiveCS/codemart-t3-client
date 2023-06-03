7fb831c5d81f5db805a5a9b70e60854d2c910115
TiveCS <ahmadfathanhanif@gmail.com>
2023-06-03 12:42:38 +0700

    change the env mjs

--- src/env.mjs
+++ src/env.mjs
@@ -5,7 +5,8 @@ import { z } from "zod";
  * built with invalid env vars.
  */
 const server = z.object({
-  DATABASE_URL: z.string().url(),
+  DATABASE_PRISMA_URL: z.string().url(),
+  DATABASE_URL_NON_POOLING: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET:
     process.env.NODE_ENV === "production"
@@ -49,7 +50,8 @@ const client = z.object({
  * @type {Record<keyof z.infer<typeof server> | keyof z.infer<typeof client>, string | undefined>}
  */
 const processEnv = {
-  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_PRISMA_URL: process.env.DATABASE_PRISMA_URL,
+  DATABASE_URL_NON_POOLING: process.env.DATABASE_URL_NON_POOLING,
   NODE_ENV: process.env.NODE_ENV,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
