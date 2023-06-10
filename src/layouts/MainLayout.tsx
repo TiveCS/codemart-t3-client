b7ce39e67159e50dfa0ba38ff2b2df0811e194b9
TiveCS <ahmadfathanhanif@gmail.com>
2023-06-10 08:36:33 +0700

    add maze snippet

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Footer from "~/components/Footer";
 import Navbar from "~/components/Navbar";
 import Toast from "~/components/Toast";
@@ -11,28 +12,57 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   const toastsStore = useToastsStore();
 
   return (
-    <div className="h-full min-h-screen">
-      <Navbar />
+    <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function (m, a, z, e) {
+              var s, t;
+              try {
+                t = m.sessionStorage.getItem('maze-us');
+              } catch (err) {}
 
-      <main className="my-8 px-6 md:px-8">{children}</main>
+              if (!t) {
+                t = new Date().getTime();
+                try {
+                  m.sessionStorage.setItem('maze-us', t);
+                } catch (err) {}
+              }
 
-      <div
-        id="toast-container"
-        className="fixed bottom-6 right-6 hidden min-w-xs md:grid md:grid-flow-row md:gap-y-4 lg:min-w-sm"
-      >
-        {toastsStore.toasts.map((toast) => (
-          <Toast
-            key={toast.id}
-            id={toast.id}
-            message={toast.message}
-            variant={toast.variant}
-            onClickHandler={() => toastsStore.closeToast(toast.id)}
-          />
-        ))}
+              s = a.createElement('script');
+              s.src = z + '?t=' + t + '&apiKey=' + e;
+              s.async = true;
+              a.getElementsByTagName('head')[0].appendChild(s);
+              m.mazeUniversalSnippetApiKey = e;
+            })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', 'de679ef3-2d5d-46e7-939e-1e4928e0a096');
+          `,
+          }}
+        />
+      </Head>
+      <div className="h-full min-h-screen">
+        <Navbar />
+
+        <main className="my-8 px-6 md:px-8">{children}</main>
+
+        <div
+          id="toast-container"
+          className="fixed bottom-6 right-6 hidden min-w-xs md:grid md:grid-flow-row md:gap-y-4 lg:min-w-sm"
+        >
+          {toastsStore.toasts.map((toast) => (
+            <Toast
+              key={toast.id}
+              id={toast.id}
+              message={toast.message}
+              variant={toast.variant}
+              onClickHandler={() => toastsStore.closeToast(toast.id)}
+            />
+          ))}
+        </div>
+
+        <Footer />
       </div>
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
